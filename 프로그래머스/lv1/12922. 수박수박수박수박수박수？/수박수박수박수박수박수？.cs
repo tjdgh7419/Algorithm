b8bd42d739864e0ed145141f895415558000eec2
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-08-24 19:42:54 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.19 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -0,0 +1,18 @@
+using System;
+public class Solution {
+    public string solution(int n) {
+        string answer = "";
+			for (int i = 0; i < n; i++)
+			{
+				if (i % 2 == 0)
+				{
+					answer += "수";
+				}
+				else
+				{
+					answer += "박";
+				}	
+			}
+			return answer;
+    }
+}
