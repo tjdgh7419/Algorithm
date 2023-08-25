a3509c2cee5fee400c0c9a92ab7882a81b99ab14
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-08-25 15:48:00 +0900

    [level 1] Title: 최소직사각형, Time: 0.39 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Solution {
+    public int solution(int[,] sizes) {
+        int answer = 0;
+			int rmax = 0;
+			int cmax = 0;
+			for(int i = 0; i < sizes.GetLength(0); i++)
+			{
+				int row = sizes[i, 1];
+				int col = sizes[i, 0];
+
+				if(col < row)
+				{
+					row = sizes[i, 0];
+					col = sizes[i, 1];
+				}
+				rmax = Math.Max(rmax, row);
+				cmax = Math.Max(cmax, col);
+			}
+			answer = rmax * cmax;
+			return answer;
+    }
+}
