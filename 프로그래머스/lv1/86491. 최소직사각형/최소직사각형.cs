0159c330e05772c38ab0575514e9a24f895deee1
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-08-25 15:58:37 +0900

    [level 1] Title: 최소직사각형, Time: 0.56 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.cs
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.cs
@@ -1,24 +1,25 @@
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 public class Solution {
     public int solution(int[,] sizes) {
-        List<int[]> list = new List<int[]>();
         int answer = 0;
-			for(int i = 0; i < sizes.GetLength(0); i++)
-			{
-				int tmp = 0;
+            int rmax = 0;
+            int cmax = 0;
+            for(int i = 0; i < sizes.GetLength(0); i++)
+            {
+                int row = sizes[i, 1];
+                int col = sizes[i, 0];
 
-				if(sizes[i, 1] > sizes[i, 0])
-				{
-					tmp = sizes[i, 1];
-					sizes[i, 1] = sizes[i, 0];
-					sizes[i, 0] = tmp;
-				}
-			    list.Add(new int[2] { sizes[i, 0], sizes[i, 1] });
-			}
-			answer = list.Max(x => x[0]) * list.Max(x => x[1]);
-			return answer;
+                if(col < row)
+                {
+                    row = sizes[i, 0];
+                    col = sizes[i, 1];
+                }
+                rmax = Math.Max(rmax, row);
+                cmax = Math.Max(cmax, col);
+            }
+            answer = rmax * cmax;
+            return answer;
     }
 }
