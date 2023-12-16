f288c1c6323d5611d65d3d631754e7fbccafce96
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-12-16 13:48:40 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2284 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	char cur;
+	vector<char>vec;
+	int rCnt = 0;
+	int lCnt = 0;
+	int sum = 0;
+	char prev = ' ';
+	while ((cur = getchar()) != '\n')
+	{
+		vec.push_back(cur);
+	}
+
+	for (int i = vec.size() - 1; i >= 0; i--)
+	{
+		char comp = vec[i];
+
+		if (prev == '(' && comp == '(')
+		{		
+			rCnt--;
+			sum += 1;
+			prev = comp;
+		}
+		else if (prev == '(' && comp == ')')
+		{
+			rCnt++;
+			prev = comp;
+		}
+		else if (prev == ')' && comp == ')')
+		{
+			rCnt++;
+			prev = comp;
+		}
+		else if (prev == ')' && comp == '(')
+		{
+			rCnt--;
+			sum += rCnt;		
+			prev = comp;
+		}
+		else {
+			rCnt++;
+			prev = comp;
+		}
+
+		vec.pop_back();
+	}
+
+	cout << sum;
+}
