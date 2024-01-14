badb6584cc109f0058eb59f035d9926172276a84
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2024-01-14 10:21:53 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -15,42 +15,33 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
-	
+
 	int t = 0;
-	
+
 	cin >> t;
 
 	for (int i = 0; i < t; i++)
 	{
 		int n;
 		cin >> n;
-		
+
 		tri[1] = 1;
 		tri[2] = 1;
 		tri[3] = 1;
 		tri[4] = 2;
 		tri[5] = 2;
 
-		if (n < 6)
+		if (n < 6 || tri[n] != 0)
 		{
 			cout << tri[n] << '\n';
 			continue;
 		}
 
-		if (tri[n] == 0) 
+		for (int j = 6; j <= n; j++)
 		{
-			for (int j = 6; j <= n; j++)
-			{
-				tri[j] = tri[j - 5] + tri[j - 1];
-			}
+			tri[j] = tri[j - 5] + tri[j - 1];
+		}
 
-			cout << tri[n] << '\n';
-			continue;
-		}
-		else
-		{
-			cout << tri[n] << '\n';
-			continue;
-		}
+		cout << tri[n] << '\n';
 	}
 }
