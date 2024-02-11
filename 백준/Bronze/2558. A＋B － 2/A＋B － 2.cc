7a476eb4e953f034dc4ba13677fb1e13629353df
Sungho Kang <70570791+tjdgh7419@users.noreply.github.com>
2024-02-11 09:33:39 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <queue>
+#include <string.h>
+#include <limits.h>
+#include <cstdio>
+
+using namespace std;
+int arr[101];
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N, a;
+	cin >> N >> a;
+
+	cout << N + a;
+
+	
+}
