fed937316f8fa5bcdb481abc3e21a63f52f69f77
Sungho Kang <70570791+tjdgh7419@users.noreply.github.com>
2024-02-24 23:06:31 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <queue>
+#include <string.h>
+#include <limits.h>
+#include <cstdio>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	string n;
+	
+	cin >> n;
+
+	cout << n.length();
+}
+
