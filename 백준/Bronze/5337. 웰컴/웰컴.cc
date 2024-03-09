e9ce62266d81c61bbe64ba083537d1c5bc4ec5fb
Sungho Kang <70570791+tjdgh7419@users.noreply.github.com>
2024-03-10 01:13:09 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <queue>
+#include <string.h>
+#include <limits.h>
+#include <cstdio>
+#include <map>
+
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+
+	cout << ".  .   ." << '\n';
+	cout << "|  | _ | _. _ ._ _  _" << '\n';
+	cout << "|/\\|(/.|(_.(_)[ | )(/.";
+
+
+}
