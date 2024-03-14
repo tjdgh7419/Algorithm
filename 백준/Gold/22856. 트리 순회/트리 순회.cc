c1e50bae2c43b9330388b713aed23b57eaa0d85a
Sungho Kang <70570791+tjdgh7419@users.noreply.github.com>
2024-03-14 11:16:59 +0900

    [Gold IV] Title: 트리 순회, Time: 32 ms, Memory: 4384 KB -BaekjoonHub

---
+++ 백준/Gold/22856. 트리 순회/트리 순회.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <queue>
+#include <string.h>
+#include <limits.h>
+#include <cstdio>
+#include <map>
+
+using namespace std;
+vector<pair<int, int>> parent(100001);
+
+int tree_node = -1;
+int right_node = -1;
+
+void inorder(int node, bool flag)
+{
+	if (node == -1) return;
+
+	int left = parent[node].first;
+	int right = parent[node].second;
+	tree_node++;
+	inorder(left, 0);
+
+	if (flag == 1)
+	{
+		right_node++;
+		inorder(right, 1);
+	}
+	else
+	{
+		inorder(right, 0);
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		int p, l, r;
+		cin >> p >> l >> r;
+
+		parent[p].first = l;
+		parent[p].second = r;
+	}
+
+	inorder(1, 1);
+	cout << (tree_node * 2) - right_node;
+}
