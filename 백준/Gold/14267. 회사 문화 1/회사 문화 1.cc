49e5248d749a93084f0ff40aed24b43c07fc730a
Sungho Kang <70570791+tjdgh7419@users.noreply.github.com>
2024-03-19 14:24:34 +0900

    [Gold IV] Title: 회사 문화 1, Time: 28 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.cc
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.cc
@@ -11,42 +11,12 @@
 
 using namespace std;
 
-vector<vector<int>> link(100001);
 int p_num[100001];
 int dp[100001];
+int n, m;
 
-void dfs(int st)
+void solve()
 {
-	for (int next : link[st])
-	{
-		if (p_num[st] == next) continue;
-		dp[next] += dp[st];
-		dfs(next);
-	}
-}
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
-	int n, m;
-	cin >> n >> m;
-
-	for (int i = 1; i <= n; i++)
-	{
-		int n_num;
-		cin >> n_num;
-
-		p_num[i] = n_num;
-
-		if (n_num != -1)
-		{
-			link[n_num].push_back(i);
-			link[i].push_back(n_num);
-		}
-	}
-
 	for (int j = 0; j < m; j++)
 	{
 		int i, w;
@@ -55,10 +25,28 @@ int main() {
 		dp[i] += w;
 	}
 
-	dfs(1);
+	cout << dp[0] << " ";
+	for (int i = 2; i <= n; i++)
+	{
+		dp[i] += dp[p_num[i]];
+		cout << dp[i] << " ";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n >> m;
 
 	for (int i = 1; i <= n; i++)
 	{
-		cout << dp[i] << " ";
+		int n_num;
+		cin >> n_num;
+
+		p_num[i] = n_num;
 	}
+
+	solve();
 }
