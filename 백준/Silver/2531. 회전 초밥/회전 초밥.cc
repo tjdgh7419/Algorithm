b648ca4535ca448fc84d72eb6617828c85aab33c
Sungho Kang <70570791+tjdgh7419@users.noreply.github.com>
2024-04-05 15:16:58 +0900

    [Silver I] Title: 회전 초밥, Time: 4 ms, Memory: 3204 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.cc
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.cc
@@ -11,37 +11,38 @@
 
 using namespace std;
 
+int dish[300001];
+int sushi[3001];
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
 
-	int N, d, k, c, ans = 0, coupon = 1, dup = 0;
+	int N, d, k, c, ans = 0, cnt = 0, coupon = 1;
 
 	cin >> N >> d >> k >> c;
-	
-	vector<int> sushi(N + 1);
-	vector<bool>visited(d + 1, false);
 
-	for (int i = 0; i < N; i++) cin >> sushi[i];
+	for (int i = 0; i < N; i++) cin >> dish[i];
 
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < k; i++)
 	{
-		dup = 0;
-		coupon = 1;
-
-		for (int j = i; j < i + k; j++)
-		{
-			if (visited[sushi[j % N]]) dup++;
-			else visited[sushi[j % N]] = true;
-
-			if (sushi[j % N] == c) coupon = 0;
-		}
-
-		ans = max(ans, k - dup + coupon);
-		fill(visited.begin(), visited.end(), false);
+		if (++sushi[dish[i]] == 1) cnt++;
 	}
 
+	if (++sushi[c] == 1) cnt++;
+
+	int start = 0, end = k - 1;
+
+	while (start < N)
+	{
+		if (--sushi[dish[start++]] == 0) cnt--;
+		
+		end = (end + 1) % N;
+
+		if (++sushi[dish[end]] == 1) cnt++;
+
+		ans = max(ans, cnt);
+	}
 	cout << ans;
 }
